215defff471cfeaed54a342055007bdcb704984c
Vitaly Bushaev <sid364@gmail.com>
2017-02-11 18:44:29 +0300

    introduce API connector

--- AdminPage.py
+++ AdminPage.py
@@ -1,8 +1,9 @@
+import logging
 class AdminPage:
     def __init__(self, bot):
         self.admins = [82755431]
         self.bot = bot
-    
+
     def add_admin(self, id):
         self.admins.append(id)
 
@@ -10,8 +11,20 @@ class AdminPage:
         self.bot.sendMessage(admin_list[0], msg)
 
     def notify_all(self, msg):
-        for admin_id in admins:
+        for admin_id in self.admins:
             self.bot.sendMessage(admin_id, msg)
 
     def is_admin(self, id):
         return id in self.admins
+
+    def handle_api_error(self, error_code, flag=False):
+        if flag:
+            msg = "Connection refused 3 times in a row with code {0}".format(error_code)
+            logging.critical(msg)
+            self.notify_all(msg)
+            return
+
+        if  401 <= error_code <= 404:
+            self.notify_all("connection to api refused with error {0}".format(error_code))
+            logging.critical("Connection to API refused with error {0}".format(error_code))
+        logging.error("Connection to API refused with error {0}".format(error_code))
